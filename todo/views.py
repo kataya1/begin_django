fdb7649c51ffafbb5b60a6f046b4dd7eab02014b
kataya1 <mohamedkataya1@gmail.com>
2021-11-06 01:26:01 +0200

    something is wrong with delete

--- todo/views.py
+++ todo/views.py
@@ -25,12 +25,34 @@ my_task_list = [
         'priority': 2,
         'description': "hello iam studying at iti sahdjka shkdj sahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffff",
     },
+    {
+        'index': 2,
+        'id': 4,
+        'name': 'Movie-3',
+        'priority': 2,
+        'description': "hello iam studying at iti sahdjka shkdj sahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffff",
+    },
+    {
+        'index': 2,
+        'id': 5,
+        'name': 'Movie-5',
+        'priority': 2,
+        'description': "hello iam studying at iti sahdjka shkdj sahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffff",
+    },
+    {
+        'index': 2,
+        'id': 6,
+        'name': 'Movie-3',
+        'priority': 2,
+        'description': "hello iam stsdfasffffffffadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffffhsadjksahdjkashkdjsahjkfghdadsgfadfadsgasdfasfffffffffffff",
+    },
 ]
 
 def _get_target_task(target_id):
     # Filter the list based on the task id sent and compare it toward each dictionary item in the list
     filter_result = filter(lambda d: d.get('id') == target_id, my_task_list)
     final_list = list(filter_result)
+    # print(f"\n\n{final_list}\n\n")
     # Getting index of the required task from my_task_list
     if len(final_list) == 0:
         return -1
@@ -55,12 +77,22 @@ def index(request):
 
 
 def tasks(request, **kwargs):
-    if request.method == 'DELETE':
-        print('\n\n-------------\nit wonts to be deleted\n\n')
-
+    print(f"taskid --> {kwargs.get('task_id')}")
     t = _get_target_task(kwargs.get('task_id'))
     if t == -1:
         return HttpResponse("404 man, there is no task with that id")
+
+    if request.method == 'POST':
+        p = request.POST
+        if p.get('op') == 'DELETE':
+            print('\n\n-------------\nit wonts to be deleted\n\n')
+            my_task_list.pop(t)
+            return redirect('index')
+
+        elif p.get('op') == 'UPDATE':
+            print('\n\n-------------\nit wonts to be updated\n\n')
+            
+
 
     return render(request, 'task.html', context=my_task_list[t])
 
